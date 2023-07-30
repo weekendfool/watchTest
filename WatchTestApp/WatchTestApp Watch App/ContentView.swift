c5e5b4c502f7970779b687207bb7b5d0a2cd12de
尾原徳泰 <nori.tukareta@gmail.com>
2023-07-30 23:06:24 +0900

    テスト用実装

---
+++ WatchTestApp/WatchTestApp Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  WatchTestApp Watch App
+//
+//  Created by Oh!ara on 2023/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("AR!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchTestApp/WatchTestApp Watch App/WatchTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchTestAppApp.swift
+//  WatchTestApp Watch App
+//
+//  Created by Oh!ara on 2023/07/28.
+//
+
+import SwiftUI
+
+@main
+struct WatchTestApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
